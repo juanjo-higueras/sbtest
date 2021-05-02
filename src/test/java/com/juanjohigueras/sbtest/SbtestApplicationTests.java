f3a928af027d3ddc59a179cf90176ea4ed30045c
Juanjo Higueras <juanjo.higueras@gmail.com>
2021-05-02 18:01:22 +0200

    Primer commit

---
+++ src/test/java/com/juanjohigueras/sbtest/SbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanjohigueras.sbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
